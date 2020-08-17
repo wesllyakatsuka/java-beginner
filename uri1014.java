83721e10ac149ab9735150fbab5a99b4276dc169
wesllyakatsuka <wesllyakatsuka@gmail.com>
2020-08-17 00:00:22 -0300

    1010 e 1014 finalizados

--- uri1014.java
+++ uri1014.java
@@ -1,9 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+//Antes de submeter esse exercício na plataforma uri judge, mudar on-line o nome da classe para Main.
 
 public class uri1014 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		int km;
+	    double litros, consumo;
+
+	    km = sc.nextInt();
+	    litros = sc.nextDouble();
+
+	    consumo = km / litros;
+
+	    System.out.printf("%.3f km/l%n", consumo);
+		
+		sc.close();
 	}
-
 }
