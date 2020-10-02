7fc5c4317fb596bb85b33e2f358b09968f365378
wesllyakatsuka <wesllyakatsuka@gmail.com>
2020-10-02 02:21:44 -0300

    1044

---
+++ uri1044.java
@@ -0,0 +1,11 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+//Antes de submeter esse exercício na plataforma uri judge, mudar on-line o nome da classe para Main.
+public class uri1044 {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
