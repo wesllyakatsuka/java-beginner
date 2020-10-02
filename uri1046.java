5e3f15a0b1f53eabd659cd505e06bad4a9b68c40
wesllyakatsuka <wesllyakatsuka@gmail.com>
2020-10-02 03:38:19 -0300

    estrutura condicional

---
+++ uri1046.java
@@ -0,0 +1,27 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+//Antes de submeter esse exercício na plataforma uri judge, mudar on-line o nome da classe para Main.
+public class uri1046 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+
+		int horaInicial = sc.nextInt();
+		int horaFinal = sc.nextInt();
+		
+		int duracao;
+		if (horaInicial < horaFinal) {
+			duracao = horaFinal - horaInicial;
+		}
+		else {
+			duracao = 24 - horaInicial + horaFinal;
+		}
+		
+		System.out.println("O JOGO DUROU " + duracao + " HORA(S)");
+		
+		sc.close();
+	}
+
+}
