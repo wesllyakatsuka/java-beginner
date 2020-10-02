db721fc90b4cef25e553da849e1a53f9a41f04ec
wesllyakatsuka <wesllyakatsuka@gmail.com>
2020-10-02 02:54:40 -0300

    concluido 1035, 1038, 1044

--- uri1044.java
+++ uri1044.java
@@ -6,6 +6,19 @@ public class uri1044 {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		if ((a % b == 0) || (b % a == 0)) {
+			System.out.println("Sao Multiplos");
+		}
+		else {
+			System.out.println("Nao sao Multiplos");
+		}
+		sc.close();
+		
 	}
 
 }
