577105b21696a593af0cae8a9e37a3e23330f866
wesllyakatsuka <wesllyakatsuka@gmail.com>
2020-10-02 02:18:04 -0300

    ajustes no diretorio

--- uri1038.java
+++ uri1038.java
@@ -2,7 +2,7 @@ import java.util.Locale;
 import java.util.Scanner;
 
 //Antes de submeter esse exercício na plataforma uri judge, mudar on-line o nome da classe para Main.
-public class uri1035 {
+public class uri1038 {
 
 	public static void main(String[] args) {
 		
