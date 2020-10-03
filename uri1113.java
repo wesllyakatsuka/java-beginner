9b8143757348613ec7d5935c50c9cadd5c0bc2d4
wesllyakatsuka <wesllyakatsuka@gmail.com>
2020-10-03 19:42:30 -0300

    Estrutura de repeticao (while)

---
+++ uri1113.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class uri1113 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);		
+		int x, y;		
+		x = sc.nextInt();
+		y = sc.nextInt();
+		
+		while(x != y) {
+			if(x < y) {
+				System.out.println("Crescente");
+			}
+			else {
+				System.out.println("Decrescente");
+			}
+			x = sc.nextInt();
+			y = sc.nextInt();
+		}	
+		sc.close();
+	}
+
+}
---
+++ uri1114.java
@@ -0,0 +1,9 @@
+
+public class uri1114 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
