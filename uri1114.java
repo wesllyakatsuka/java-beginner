c13c0e9040c0ac53d75ac4e69de763f16b26f6cc
wesllyakatsuka <wesllyakatsuka@gmail.com>
2020-10-03 21:32:25 -0300

    Estrutura de repeticao (while)

--- uri1114.java
+++ uri1114.java
@@ -1,9 +1,17 @@
+import java.util. Scanner;
 
 public class uri1114 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);
+		int senha = sc.nextInt();
+		
+		while(senha != 2002) {
+			System.out.println("Senha Invalida");
+			senha = sc.nextInt();
+		}
+		System.out.println("Acesso Permitido");
+		sc.close();		
 	}
-
 }
