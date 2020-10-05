75e58e8f1fef1a93fb658055f38da30fedaf5574
wesllyakatsuka <wesllyakatsuka@gmail.com>
2020-10-05 01:59:33 -0300

    Estrutura de repeticao (For)

---
+++ uri1078.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class uri1078 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		for(int i = 1; i <= 10; i++) {
+			int produto = i * n;
+			System.out.println(i + " x " + n + " = " + produto);
+		}
+			sc.close();	
+	}
+
+}
